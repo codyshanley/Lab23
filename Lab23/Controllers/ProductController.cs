5d92293ae2c559d3817b6ee23ac5659962bfd5e5
codyshanley <cody.shanley@gmail.com>
2019-11-19 18:09:19 -0500

    first commit

---
+++ Lab23/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using Lab23.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab23.Controllers
+{
+    public class ProductController : Controller
+    {
+        ShopContext db = new ShopContext();
+
+        public IActionResult Index()
+        {
+            List<Products> products = db.Products.ToList();
+            return View(db.Products.ToList());
+        }
+
+        public IActionResult Buy(int id)
+        {
+            Products p = db.Products.Find(id);
+            if (p != null)
+            {
+                return View(p);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Product");
+            }
+        }
+    }
+}
